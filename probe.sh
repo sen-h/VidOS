02abd116b3dd9103706a7087f5ab534d0aae50c6
Paul Hastings <plh@sonic.net>
2021-07-12 22:27:42 -0500

    added more build script logic, moved to base config structure with variants added as patch files. also reordered things in probe script

--- probe.sh
+++ probe.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-BUILDROOT_LATEST=buildroot-2021.02.1
+BUILDROOT_LATEST=buildroot-2021.05
 VID_CODEC=$(./ffprobe -hide_banner -loglevel 0 -show_streams $1 | grep -w codec_name | sed -n 1p)
 AUD_CODEC=$(./ffprobe -hide_banner -loglevel 0 -show_streams $1 | grep -w codec_name | sed -n 2p)
+
+if [ $AUD_CODEC = "codec_name=opus" ]; then
+	echo "audio passed"
+else
+	echo "Warning: audio is not encoded in opus, and will not play back!"
+fi
 
 if [ $VID_CODEC = "codec_name=av1" ]; then
 	echo "video passed"
@@ -14,10 +20,4 @@ if [ $VID_CODEC = "codec_name=av1" ]; then
 	CD_root
 elif [ $VID_CODEC != "codec_name=av1" ]; then
 	echo "Error: Video is not encoded in AV1, please try again!"
-fi
-
-if [ $AUD_CODEC = "codec_name=opus" ]; then
-	echo "audio passed"
-else
-	echo "Warning: audio is not encoded in opus, and will not play back!"
 fi
