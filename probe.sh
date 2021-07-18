5f734bc30fbdc576a7f2e706bb9a23b325d195e3
Paul Hastings <plh@sonic.net>
2021-07-18 12:02:54 -0500

    added logic to discern between different builds of vidos as well as some cleanup

--- probe.sh
+++ probe.sh
@@ -5,7 +5,7 @@ VID_CODEC=$(./ffprobe -hide_banner -loglevel 0 -show_streams $1 | grep -w codec_
 AUD_CODEC=$(./ffprobe -hide_banner -loglevel 0 -show_streams $1 | grep -w codec_name | sed -n 2p | cut -d'=' -f2)
 SUPPORTED_VID_CODECS=("av1" "vp8" "vp9")
 IS_SUPPORTED=0
-
+VIDOS_ROOTFS=$(ls $BUILDROOT_LATEST/output/images/ | grep -m 1 "vidos_rootfs_*")
 if [ $AUD_CODEC = "opus" ]; then
 	echo "audio passed"
 else
@@ -19,18 +19,28 @@ do
 	fi
 done
 
+if [ -z $VIDOS_ROOTFS ]; then
+	echo "vidos root filesystem has not been built! Please run build.sh."
+	exit 1
+elif [ $VIDOS_ROOTFS != "vidos_rootfs_"$VID_CODEC ]; then
+	echo "video is not of correct type for currently built VidOS"
+	echo "video requires vidos_"$VID_CODEC
+	exit 1
+fi
+
 if [ $IS_SUPPORTED = 1 ]; then
 	echo "video passed"
-	cp $1 $BUILDROOT_LATEST/output/images/CD_root/video/
-	ls $BUILDROOT_LATEST/output/images/CD_root/video/ > $BUILDROOT_LATEST/output/images/playlist.txt
-	cp $BUILDROOT_LATEST/output/images/playlist.txt $BUILDROOT_LATEST/output/images/CD_root/video/
+	cp $1 $BUILDROOT_LATEST/output/images/$VIDOS_ROOTFS/video/
+	ls $BUILDROOT_LATEST/output/images/$VIDOS_ROOTFS/video/ > $BUILDROOT_LATEST/output/images/playlist.txt
+	cp $BUILDROOT_LATEST/output/images/playlist.txt $BUILDROOT_LATEST/output/images/$VIDOS_ROOTFS/video/
 	echo "installed "$1" as a new video and rebuilt playlist"
 	cd $BUILDROOT_LATEST/output/images/
 	echo "rebuilding iso"
-	xorriso -as mkisofs -o output.iso -isohybrid-mbr isohdpfx.bin \
+	xorriso -as mkisofs -o vidos_$VID_CODEC.iso -isohybrid-mbr isohdpfx.bin \
 	-c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table \
-	CD_root
+	$VIDOS_ROOTFS
 else
-	echo "Error: video is encoded in "$VID_CODEC", which is not supported!"
-	echo "Please use a video encoded in one of the following formats: "${SUPPORTED_VID_CODECS[@]}
+	echo "video is not of correct type for currently built VidOS"
+        echo $VID_CODEC" is not a supported video format, please specify one of the following video formats: "${SUPPORTED_VID_CODECS[@]}
+	exit 1
 fi
--- vidos_av1/syslinux_post-image.sh
+++ vidos_av1/syslinux_post-image.sh
@@ -3,30 +3,32 @@ set -e
 
 BOARD_DIR=$(dirname "$0")
 
+VIDOS_ROOTFS=vidos_rootfs_$2
+
 export $(cat $PWD/boot/syslinux/syslinux.mk | grep "SYSLINUX_VERSION ="| sed 's: ::g')
 export SYSLINUX=syslinux-$SYSLINUX_VERSION
 export SYSLINUX_PATH=$BUILD_DIR/$SYSLINUX
 echo "found syslinux version: "$SYSLINUX_PATH
-mkdir -p $BINARIES_DIR/CD_root/isolinux
-mkdir -p $BINARIES_DIR/CD_root/kernel
-mkdir -p $BINARIES_DIR/CD_root/video
-mkdir -p $BINARIES_DIR/CD_root/firmware
-echo "created CD_root directory"
-cp $BOARD_DIR/isolinux.cfg $BINARIES_DIR/CD_root/isolinux
-cp $BOARD_DIR/video.mkv $BINARIES_DIR/CD_root/video/video.mkv
-ls $BINARIES_DIR/CD_root/video/ > $BINARIES_DIR/playlist.txt
-cp $BINARIES_DIR/playlist.txt $BINARIES_DIR/CD_root/video/
-cp $BINARIES_DIR/syslinux/isolinux.bin $BINARIES_DIR/CD_root/isolinux
+mkdir -p $BINARIES_DIR/$VIDOS_ROOTFS/isolinux
+mkdir -p $BINARIES_DIR/$VIDOS_ROOTFS/kernel
+mkdir -p $BINARIES_DIR/$VIDOS_ROOTFS/video
+mkdir -p $BINARIES_DIR/$VIDOS_ROOTFS/firmware
+echo "created $VIDOS_ROOTFS directory"
+cp $BOARD_DIR/isolinux.cfg $BINARIES_DIR/$VIDOS_ROOTFS/isolinux
+cp $BOARD_DIR/video.mkv $BINARIES_DIR/$VIDOS_ROOTFS/video/video.mkv
+ls $BINARIES_DIR/$VIDOS_ROOTFS/video/ > $BINARIES_DIR/playlist.txt
+cp $BINARIES_DIR/playlist.txt $BINARIES_DIR/$VIDOS_ROOTFS/video/
+cp $BINARIES_DIR/syslinux/isolinux.bin $BINARIES_DIR/$VIDOS_ROOTFS/isolinux
 cp $SYSLINUX_PATH/bios/mbr/isohdpfx.bin $BINARIES_DIR
-cp $SYSLINUX_PATH/bios/com32/elflink/ldlinux/ldlinux.c32 $BINARIES_DIR/CD_root/isolinux
-cp -r $BINARIES_DIR/amdgpu $BINARIES_DIR/CD_root/firmware
-cp -r $BINARIES_DIR/radeon $BINARIES_DIR/CD_root/firmware
-cp -r $BINARIES_DIR/i915 $BINARIES_DIR/CD_root/firmware
-cp $BINARIES_DIR/bzImage $BINARIES_DIR/CD_root/kernel
-echo "copied files into CD_root directory"
+cp $SYSLINUX_PATH/bios/com32/elflink/ldlinux/ldlinux.c32 $BINARIES_DIR/$VIDOS_ROOTFS/isolinux
+cp -r $BINARIES_DIR/amdgpu $BINARIES_DIR/$VIDOS_ROOTFS/firmware
+cp -r $BINARIES_DIR/radeon $BINARIES_DIR/$VIDOS_ROOTFS/firmware
+cp -r $BINARIES_DIR/i915 $BINARIES_DIR/$VIDOS_ROOTFS/firmware
+cp $BINARIES_DIR/bzImage $BINARIES_DIR/$VIDOS_ROOTFS/kernel
+echo "copied files into $VIDOS_ROOTFS directory"
 cd $BINARIES_DIR
 
-xorriso -as mkisofs -o output.iso -isohybrid-mbr isohdpfx.bin \
+xorriso -as mkisofs -o vidos_$2.iso -isohybrid-mbr isohdpfx.bin \
   -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table \
-  CD_root
+  $VIDOS_ROOTFS/
 
