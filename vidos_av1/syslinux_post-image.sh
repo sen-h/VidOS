c7dda27216a62bebad22ac84c6a6a0b354fd9091
sen-h <sen.hastings@indigoblueinnovations.com>
2021-10-18 14:49:31 -0500

    added release dir logic to syslinux_post-image.sh

--- vidos_av1/syslinux_post-image.sh
+++ vidos_av1/syslinux_post-image.sh
@@ -7,9 +7,9 @@ VIDOS_ISO9660=vidos_iso9660_$2
 
 VIDOS_BUILD=$2_build
 
-VIDOS_ISO9660_PATH=$BINARIES_DIR/$VIDOS_BUILD/$VIDOS_ISO9660
-
-VIDOS_BUILD_PATH=$BINARIES_DIR/$VIDOS_BUILD/
+VIDOS_RELEASE_PATH=$BINARIES_DIR/vidos_release/
+VIDOS_BUILD_PATH=$BINARIES_DIR/vidos_release/$VIDOS_BUILD/
+VIDOS_ISO9660_PATH=$BINARIES_DIR/vidos_release/$VIDOS_BUILD/$VIDOS_ISO9660
 
 export $(cat $PWD/boot/syslinux/syslinux.mk | grep "SYSLINUX_VERSION ="| sed 's: ::g')
 export SYSLINUX=syslinux-$SYSLINUX_VERSION
@@ -22,7 +22,8 @@ echo "created $VIDOS_ISO9660 directory"
 
 cp -r $BOARD_DIR/initramfs_overlay $VIDOS_BUILD_PATH
 cp $BOARD_DIR/S03Video* $VIDOS_BUILD_PATH
-cp $BOARD_DIR/vobu.sh $VIDOS_BUILD_PATH
+cp $BOARD_DIR/vobu.sh $VIDOS_RELEASE_PATH
+
 cp $BOARD_DIR/isolinux.cfg $VIDOS_ISO9660_PATH/isolinux
 cp $BOARD_DIR/video.mkv $VIDOS_ISO9660_PATH/video/video.mkv
 ls $VIDOS_ISO9660_PATH/video/ > $BINARIES_DIR/playlist.txt
@@ -39,4 +40,3 @@ cd $BINARIES_DIR
 xorriso -as mkisofs -o vidos_$2.iso -isohybrid-mbr isohdpfx.bin \
   -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table \
   $VIDOS_ISO9660_PATH/
-
