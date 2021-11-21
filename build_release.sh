c3ebaf23dfc0f7859a26a064b3850f8135017ff8
sen-h <plh@sonic.net>
2021-11-20 22:54:56 -0600

    changed from codec to format paradigm, also updated readme and modified release buildout

--- build_release.sh
+++ build_release.sh
@@ -4,20 +4,13 @@ SUPPORTED_FORMATS=("av1" "avc" "webm")
 SUPPORTED_STYLES=("disk" "ram" "hybrid")
 mkdir -p vidos_release/vidos_components
 
-#cp -r $BUILDROOT_LATEST/${SUPPORTED_FORMATS[0]}/images/vidos_release/${SUPPORTED_FORMATS[0]}"_build" vidos_release/vidos_components
 cp -r $BUILDROOT_LATEST/${SUPPORTED_FORMATS[0]}/images/vidos_release/${SUPPORTED_FORMATS[0]}"_build"/. vidos_release/vidos_components
 
 for FORMAT in "${SUPPORTED_FORMATS[@]}"; do
 	echo $BUILDROOT_LATEST/$FORMAT/images/vidos_release/$FORMAT"_build"/$FORMAT"_kernel"
 	cp -r $BUILDROOT_LATEST/$FORMAT/images/vidos_release/$FORMAT"_build"/$FORMAT"_kernel" vidos_release/vidos_components
-#	cp -r vidos_av1/S03Video_ram vidos_release/vidos_components
-#	cp -r vidos_av1/S03Video_disk vidos_release/vidos_components
-#	cp -r vidos_av1/S03Video_hybrid vidos_release/vidos_components
 
 done
-#for STYLE in "${SUPPORTED_STYLES[@]}"; do
-#	cp -r vidos_av1/S03Video_$STYLE vidos_release/vidos_components
-#done
 
 cp vidos_av1/vobu.sh vidos_release/
 cp README.md vidos_release/
