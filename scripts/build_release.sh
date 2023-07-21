0befb0c52315276b232f34fc1d8fce42757e97bf
sen-h <plh@sonic.net>
2023-07-21 00:07:27 -0700

    updated paths and added more stuff to build_release.sh

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+VIDOS_VER="2.00"
 BUILDROOT_LATEST="buildroot-2023.05.1"
 KERNEL_LATEST="5.4.249"
 KERNEL_LATEST_MAJOR=$(echo $KERNEL_LATEST | head -c 1)
@@ -9,7 +10,7 @@ NAME=$1
 
 if [ -z $NAME ]; then NAME=$(date +%F); fi
 
-mkdir -p vidos_release_$NAME/vidos_components
+mkdir -p vidos_release_$NAME/vidos_components-v$VIDOS_VER
 mkdir -p vidos_release_$NAME-source-and-licence-info/
 
 pushd $BUILDROOT_LATEST
@@ -22,10 +23,10 @@ for FORMAT in "${SUPPORTED_FORMATS[@]}"; do
 	cp -r $FORMAT/legal-info/buildroot.config ../vidos_release_$NAME-source-and-licence-info/$FORMAT-buildroot.config
 
 	echo $FORMAT/images/vidos_release/$FORMAT"_build"/$FORMAT"_kernel"
-	cp -r $FORMAT/images/vidos_release/$FORMAT"_build"/* ../vidos_release_$NAME/vidos_components
+	cp -r $FORMAT/images/vidos_release/$FORMAT"_build"/* ../vidos_release_$NAME/vidos_components-v$VIDOS_VER
 done
 
-	mv ../vidos_release_$NAME-source-and-licence-info/host-source ../vidos_release_$NAME-source-and-licence-info/combined-host-sources
+	mv ../vidos_release_$NAME-source-and-licence-info/host-sources ../vidos_release_$NAME-source-and-licence-info/combined-host-sources
 	mv ../vidos_release_$NAME-source-and-licence-info/host-licenses ../vidos_release_$NAME-source-and-licence-info/combined-host-licenses
 	mv ../vidos_release_$NAME-source-and-licence-info/sources ../vidos_release_$NAME-source-and-licence-info/combined-sources
 	mv ../vidos_release_$NAME-source-and-licence-info/licenses ../vidos_release_$NAME-source-and-licence-info/combined-licenses
@@ -36,10 +37,10 @@ done
 
 popd
 
-cp vidos_av1/vobu.sh README.md vidos_release_$NAME/
+cp vidos_av1/vobu.sh README.md release_paperwork/uninstall.sh release_paperwork/install.sh vidos_release_$NAME/
 
 wget https://cdn.kernel.org/pub/linux/kernel/v$KERNEL_LATEST_MAJOR.x/linux-$KERNEL_LATEST.tar.xz -O vidos_release_$NAME-source-and-licence-info/linux-$KERNEL_LATEST.tar.xz
 wget https://buildroot.org/downloads/$BUILDROOT_LATEST.tar.xz -O  vidos_release_$NAME-source-and-licence-info/$BUILDROOT_LATEST.tar.xz
 find vidos_release_$NAME -type f -name "rootfs.cpio.lz4" -delete
-rm vidos_release_$NAME/vidos_components/vidos_iso9660/video/*
-rm -r vidos_release_$NAME/vidos_components/avc_external_lib
+rm vidos_release_$NAME/vidos_components-v$VIDOS_VER/vidos_iso9660/video/*
+rm -r vidos_release_$NAME/vidos_components-v$VIDOS_VER/avc_external_lib
