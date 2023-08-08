aa77277d4bc36bbcdaceccd457aea4af6cc0e24a
sen-h <plh@sonic.net>
2023-08-08 15:34:41 -0700

    added deploy_configs.sh and updated rebuild_patches.sh to accept a single format as an argument.

--- scripts/rebuild_patches.sh
+++ scripts/rebuild_patches.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BUILDROOT_LATEST="buildroot-2023.05.1"
-SUPPORTED_FORMATS=("av1" "avc" "webm")
+SUPPORTED_FORMATS=("av1" "avc" "webm" "efi")
 SUPPORTED_STYLES=("disk" "ram" "hybrid")
 
 #this script generates new patches for the vidos format configs
@@ -8,7 +8,7 @@ SUPPORTED_STYLES=("disk" "ram" "hybrid")
 
 pushd $BUILDROOT_LATEST
 
-for FORMAT in "${SUPPORTED_FORMATS[@]}"; do
+for FORMAT in "$@"; do
 #first savedefconfigs are created for each format
 	make O=$FORMAT -j$(nproc) savedefconfig BR2_DEFCONFIG=../configs/"vidos_"$FORMAT"_config"
 	echo "vidos_"$FORMAT"_config"
@@ -17,7 +17,7 @@ done
 popd
 pushd configs
 
-for FORMAT in "${SUPPORTED_FORMATS[@]}"; do
+for FORMAT in "$@"; do
 #then those savedefconfigs are compared against the base config to create patches
 	diff -U0 vidos_base_config  "vidos_"$FORMAT"_config" | cut -f1 > ../patches/vidos_$FORMAT.patch
 	echo "vidos_$FORMAT.patch"
